f9460884054aa950014b1630a7c8923b919693b7
DLKDOG <la68jun@gmail.com>
2024-09-21 16:37:12 +0900

    오늘수업종료

--- Ch4.cpp
+++ Ch4.cpp
@@ -1,27 +1,18 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main()
+void fun( );
+int a=0; // 외부변수 a
+int main( )
 {
-    int total;
-    int a = 10;
-    int i;
-
-    total = 0;
-    for(i = 1; i <= a; i++){
-        total += i;
-    }
-    cout << "total=" << total << endl; // std:: 제거
-
-    total = 0;
-    for(i = 1; i <= 100; i++){
-        total += i;
-    }
-    cout << "total=" << total << endl; // std:: 제거
-
-    total = 0;
-    for(i = 1; i <= 5; i++){
-        total += 1;
-    }
-    cout << "total=" << total << endl; // std:: 제거
+int a = 20; // 지역변수 a
+cout<<"\nmain a => "<<a;
+fun( );
+cout<<"\nmain a => "<<a<<"\n";
+char ch; cin>>ch;
+}
+void fun( )
+{
+cout<<"\nfun a => "<<a;
+a=a+100;
+cout<<"\nfun a => "<<a;
 }
