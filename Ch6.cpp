69750fbc2b2d8bbc674c13f81725f7a8e57294a6
DLKDOG <la68jun@gmail.com>
2024-11-10 12:15:16 +0900

    1110

---
+++ Ch6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    cout << "a의 주소: " << &a << endl;
+    cout << "b의 주소: " << &b << endl;
+
+    return 0;
+}
