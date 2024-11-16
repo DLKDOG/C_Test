026eab4eed2a38c05d9d08d85cae0d1a7c67b4a8
DLKDOG <la68jun@gmail.com>
2024-11-16 11:26:58 +0900

    1116

---
+++ ch09_01.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+private:
+    int real;
+    int image;
+public:
+    void SetComplex();
+    void ShowComplex();
+};
+
+void Complex::SetComplex()
+{
+    real = 2;
+    image = 5;
+}
+
+void Complex::ShowComplex()
+{
+    cout << "(" << real << " + " << image << "i)" << endl;
+}
+
+int main()
+{
+    Complex x, y;
+
+    x.SetComplex();
+    x.ShowComplex();
+    y.SetComplex();
+    y.ShowComplex();
+
+    char ch;
+    cin >> ch;
+
+    return 0;
+}
